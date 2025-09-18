9aa037d371f28d46a945f83251c48bf9df213ba7
Tobias Richardson <haji5.richardson@Gmail.com>
2025-09-18 00:52:40 -0700

    update

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,5 +1,4 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
@@ -40,5 +39,4 @@ module.exports = {
     },
   },
   plugins: [],
-}
-
+};
